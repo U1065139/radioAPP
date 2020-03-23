dab7d4e203fdbb6ffe05fe391888a4b9c5ac80c1
lance <lance880810@gmail.com>
2020-03-23 11:09:26 +0800

    first commit

---
+++ app/src/main/java/com/U1065139/radioapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.U1065139.radioapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
