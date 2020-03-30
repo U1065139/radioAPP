badebade3a8a0a4b7d065234c50293c5bff84414
U1065139 <lance880810@gmail.com>
2020-03-30 11:01:22 +0800

    0330

--- app/src/main/java/com/U1065139/radioapp/MainActivity.java
+++ app/src/main/java/com/U1065139/radioapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.U1065139.radioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn_ok(View view) {
+        TextView txvLike = findViewById(R.id.txvShow);
+        RadioGroup kGrilGroup = findViewById(R.id.kGirlGroup);
+
+        int id = kGrilGroup.getCheckedRadioButtonId();
+        RadioButton selected = findViewById(id);
+
+        txvLike.setText("My favorite K Girl Group:"  + selected.getText());
+
     }
 }
